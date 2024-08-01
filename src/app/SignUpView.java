c21b2a183e7957a9f75f3f99e105f0d565cb55be
Deivison Santos <deivisonddj3@gmail.com>
2024-08-01 15:35:58 -0300

    campos de input e labels

--- src/app/SignUpView.java
+++ src/app/SignUpView.java
@@ -3,27 +3,39 @@ package app;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JDesktopPane;
+
 import javax.swing.JTextField;
 import java.awt.Panel;
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JButton;
+import javax.swing.JPasswordField;
 import javax.swing.JTextPane;
-import javax.swing.DropMode;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import javax.swing.JFormattedTextField;
+import java.text.SimpleDateFormat;
+import java.text.DateFormat;
+import com.toedter.calendar.JCalendar;
 
 public class SignUpView {
 
 	private JFrame frame;
 	private JTextField nameField;
-	private JTextField textField_2;
-	private JTextField textField_1;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField emailField;
+	private JTextField addressField;
+	private JTextField cpfField;
+	private JPasswordField passwordField;
+	private JPasswordField confirmPasswordField;
 	private JTextPane txtpnBemVindoAo;
+	private JLabel namePlaceholder;
+	private JLabel emailPlaceholder;
+	private JLabel passwordPlaceholder;
+	private JLabel confirmPasswordPlaceholder;
+	private JLabel addressPlaceholder;
+	private JLabel cpfPlaceholder;
+	private JLabel lblDataDeNascimento;
 
 	/**
 	 * Launch the application.
@@ -53,72 +65,211 @@ public class SignUpView {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.setBounds(100, 100, 1170, 699);
 		frame.getContentPane().setLayout(null);
+		
+		// ------------------------- Welcome Section ------------------------- 
 		
 		Panel WelcomeSection = new Panel();
 		WelcomeSection.setBackground(new Color(192, 192, 192));
-		WelcomeSection.setBounds(0, 0, 341, 655);
+		WelcomeSection.setBounds(0, 0, 341, 660);
 		frame.getContentPane().add(WelcomeSection);
 		WelcomeSection.setLayout(null);
 		
-		txtpnBemVindoAo = new JTextPane();
+		txtpnBemVindoAo  = new JTextPane();
+		txtpnBemVindoAo.setEditable(false);
 		txtpnBemVindoAo.setBackground(new Color(192, 192, 192));
 		txtpnBemVindoAo.setFont(new Font("Tahoma", Font.PLAIN, 31));
 		txtpnBemVindoAo.setText("Bem Vindo\r\n      Ao\r\n BetterBet");
-		txtpnBemVindoAo.setBounds(92, 182, 204, 134);
+		txtpnBemVindoAo.setBounds(84, 202, 204, 134);
 		WelcomeSection.add(txtpnBemVindoAo);
 		
 		JTextPane txtpnOSiteCom = new JTextPane();
-		txtpnOSiteCom.setDropMode(DropMode.INSERT_ROWS);
+		txtpnOSiteCom.setEditable(false);
 		txtpnOSiteCom.setText("O site com as apostas seguras, lucrativas e com melhor conversão em ganhos do mercado!");
 		txtpnOSiteCom.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		txtpnOSiteCom.setBackground(Color.LIGHT_GRAY);
-		txtpnOSiteCom.setBounds(52, 327, 262, 134);
+		txtpnOSiteCom.setBounds(44, 347, 262, 134);
 		WelcomeSection.add(txtpnOSiteCom);
+		
+		
+		// ------------------------- SignUp Section ------------------------- 
 		
 		JLabel PageTitle = new JLabel("Criar Conta");
 		PageTitle.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		PageTitle.setBounds(637, 115, 191, 31);
+		PageTitle.setBounds(637, 78, 191, 31);
 		frame.getContentPane().add(PageTitle);
 		
+		// Name Field
+		namePlaceholder = new JLabel("Nome");
+		namePlaceholder.setFont(new Font("Arial", Font.PLAIN, 14));
+		namePlaceholder.setBounds(519, 173, 426, 14);
+		frame.getContentPane().add(namePlaceholder);
+		
+		
 		nameField = new JTextField();
-		nameField.setBounds(505, 208, 400, 31);
+		namePlaceholder.setLabelFor(nameField);
+		nameField.addFocusListener(new FocusAdapter() {
+			
+			// Tira o placeholder quando o usuário clicar no campo
+			@Override
+			public void focusGained(FocusEvent e) {
+				namePlaceholder.setVisible(false);
+			}
+			// Coloca o placeholder quando o usuário sair do campo e não tiver texto
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(nameField.getText().length() == 0) {
+					namePlaceholder.setVisible(true);
+				}
+			}
+		});
+		nameField.setBounds(505, 165, 440, 31);
 		frame.getContentPane().add(nameField);
 		nameField.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(505, 320, 400, 31);
-		frame.getContentPane().add(textField_2);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(505, 264, 400, 31);
-		frame.getContentPane().add(textField_1);
+		// Email Field
+		emailPlaceholder = new JLabel("Email");
+		emailPlaceholder.setFont(new Font("Arial", Font.PLAIN, 14));
+		emailPlaceholder.setBounds(519, 229, 426, 14);
+		frame.getContentPane().add(emailPlaceholder);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(505, 380, 230, 31);
-		frame.getContentPane().add(textField_3);
+		emailField = new JTextField();
+		emailPlaceholder.setLabelFor(emailField);
+		emailField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				emailPlaceholder.setVisible(false);
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(emailField.getText().length() == 0) {
+					emailPlaceholder.setVisible(true);
+				}
+			}
+		});
+		emailField.setColumns(10);
+		emailField.setBounds(505, 221, 440, 31);
+		frame.getContentPane().add(emailField);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(745, 380, 160, 31);
-		frame.getContentPane().add(textField_4);
+		// Password Field 
+		passwordPlaceholder = new JLabel("Senha");
+		passwordPlaceholder.setFont(new Font("Arial", Font.PLAIN, 14));
+		passwordPlaceholder.setBounds(519, 332, 209, 14);
+		frame.getContentPane().add(passwordPlaceholder);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(505, 433, 230, 31);
-		frame.getContentPane().add(textField_5);
+		passwordField = new JPasswordField();
+		passwordPlaceholder.setLabelFor(passwordField);
+		passwordField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				passwordPlaceholder.setVisible(false);
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(passwordField.getText().length() == 0) {
+					passwordPlaceholder.setVisible(true);
+				}
+			}
+		});
+		passwordField.setBounds(505, 324, 223, 31);
+		frame.getContentPane().add(passwordField);
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(745, 433, 160, 31);
-		frame.getContentPane().add(textField_6);
+		
+		// Confirm Password Field
+		confirmPasswordPlaceholder = new JLabel("Confirme sua senha");
+		confirmPasswordPlaceholder.setFont(new Font("Arial", Font.PLAIN, 14));
+		confirmPasswordPlaceholder.setBounds(519, 391, 209, 14);
+		frame.getContentPane().add(confirmPasswordPlaceholder);
+		
+		confirmPasswordField = new JPasswordField();
+		confirmPasswordPlaceholder.setLabelFor(confirmPasswordField);
+		confirmPasswordField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				confirmPasswordPlaceholder.setVisible(false);
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(confirmPasswordField.getText().length() == 0) {
+					confirmPasswordPlaceholder.setVisible(true);
+				}
+			}
+		});
+		confirmPasswordField.setBounds(505, 383, 223, 31);
+		frame.getContentPane().add(confirmPasswordField);
+	
+		
+		// Address Field
+		addressPlaceholder = new JLabel("Endereço");
+		addressPlaceholder.setFont(new Font("Arial", Font.PLAIN, 14));
+		addressPlaceholder.setBounds(519, 284, 426, 14);
+		frame.getContentPane().add(addressPlaceholder);
+		
+		addressField = new JTextField();
+		addressPlaceholder.setLabelFor(addressField);
+		addressField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				addressPlaceholder.setVisible(false);
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(addressField.getText().length() == 0) {
+					addressPlaceholder.setVisible(true);
+				}
+			}
+		});
+		addressField.setColumns(10);
+		addressField.setBounds(505, 276, 440, 31);
+		frame.getContentPane().add(addressField);
+		
+		
+		// CPF Field
+		cpfPlaceholder = new JLabel("CPF");
+		cpfPlaceholder.setFont(new Font("Arial", Font.PLAIN, 14));
+		cpfPlaceholder.setBounds(519, 448, 61, 14);
+		frame.getContentPane().add(cpfPlaceholder);
+		
+		cpfField = new JTextField();
+		cpfPlaceholder.setLabelFor(cpfField);
+		cpfField.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				cpfPlaceholder.setVisible(false);
+			}
+			@Override
+			public void focusLost(FocusEvent e) {
+				if(cpfField.getText().length() == 0) {
+					cpfPlaceholder.setVisible(true);
+				}
+			}
+		});
+		cpfField.setColumns(10);
+		cpfField.setBounds(505, 440, 223, 31);
+		frame.getContentPane().add(cpfField);
+		
+		
+		// BirthDate Field
+		JCalendar calendar = new JCalendar();
+		frame.getContentPane().add(calendar);
+		calendar.setBounds(745, 324, 200, 180);
+//		lblDataDeNascimento = new JLabel("Data de Nascimento");
+//		lblDataDeNascimento.setFont(new Font("Arial", Font.PLAIN, 12));
+//		lblDataDeNascimento.setBounds(750, 442, 140, 14);
+//		frame.getContentPane().add(lblDataDeNascimento);
+		
 		
 		JButton btnNewButton = new JButton("Cadastrar");
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnNewButton.setBounds(637, 519, 179, 59);
+		btnNewButton.setBounds(637, 549, 179, 59);
 		frame.getContentPane().add(btnNewButton);
+		
+		// ------------------------- SignIn Link ------------------------- 
+		JLabel lblNewLabel = new JLabel("<html><a href=''>Já Possui Conta?</a></html>");
+		lblNewLabel.setBounds(690, 618, 117, 14);
+		frame.getContentPane().add(lblNewLabel);
+			
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -7,4 +7,5 @@
 module BetterBetTest {
 	requires java.desktop;
 	requires java.sql;
+	requires jcalendar;
 }
