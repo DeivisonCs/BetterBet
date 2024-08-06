13fedc8a7eadd0f0f04d605acd2222fe454c5ac6
gds4 <gdantas536@gmail.com>
2024-08-06 14:35:46 -0300

    Corrigido bug de visualização de partidas

--- src/app/homeUser/ProfileImageComponent.java
+++ src/app/homeUser/ProfileImageComponent.java
@@ -20,10 +20,10 @@ public class ProfileImageComponent extends JPanel {
         try {
             ByteArrayInputStream bais = new ByteArrayInputStream(imageData);
             BufferedImage originalImage = ImageIO.read(bais);
-            image = new BufferedImage(50, 50, BufferedImage.TYPE_INT_ARGB);
-            Graphics2D g2d = image.createGraphics();
+            this.image = new BufferedImage(60, 60, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g2d = this.image.createGraphics();
             g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-            g2d.drawImage(originalImage, 0, 0, 50, 50, null);
+            g2d.drawImage(originalImage, 0, 0, 60, 60, null);
             g2d.dispose();
         } catch (IOException ex) {
             ex.printStackTrace();
@@ -34,7 +34,7 @@ public class ProfileImageComponent extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (image != null) {
+        if (this.image != null) {
             Graphics2D g2 = (Graphics2D) g.create();
 
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -45,7 +45,7 @@ public class ProfileImageComponent extends JPanel {
             Ellipse2D.Double circle = new Ellipse2D.Double(5, 5, 50, 50);
             g2.setClip(circle);
 
-            g2.drawImage(image, 5, 5, 50, 50, null);
+            g2.drawImage(this.image, 5, 5, 50, 50, null);
 
             g2.setClip(null);
 
